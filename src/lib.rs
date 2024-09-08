8f7e343f3860a46f610c1b0bfcd569e887178331
bahramkb <bahramkb@gmail.com>
2024-09-08 08:34:09 +0200

    Add Actix Web setup
    Refactored main function to use Actix Web async runtime and dependencies. This includes initializing the web server using run_app function, paving the way for adding endpoints such as health check. Updating Cargo.lock to include necessary Actix Web dependencies.

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use actix_web::{web, App, HttpServer};
+
+pub mod routes;
+
+#[macro_export]
+macro_rules! build_app {
+    () => {
+        App::new()
+            .route("/health_check", web::get().to($crate::routes::health_check::health_check))
+    };
+}
+
+pub async fn run_app() -> std::io::Result<()> {
+    HttpServer::new(|| { build_app!() })
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
+}
+
---
+++ src/routes/health_check.rs
@@ -0,0 +1,22 @@
+use actix_web::{HttpResponse, Responder};
+pub async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("OK")
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{test, App, web};
+
+    use crate::build_app;
+    #[actix_web::test]
+    async fn dummy_test() {
+        let app = test::init_service(build_app!()).await;
+        let req = test::TestRequest::get().uri("/health_check").to_request();
+        let response = test::call_service(&app, req).await;
+
+        assert!(response.status().is_success());
+
+        let body = test::read_body(response).await;
+        assert_eq!(body, "OK");
+    }
+}
